fd734ad02f5ae6d0a17329151630cb997470a0a6
Yuriy Piddubnyak <banzaj@gmail.com>
2020-04-13 16:51:14 +0300

    Pass variables via query

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,17 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController extends AbstractController
 {
     /**
-     * @Route("/welcome", name="welcome")
+     * @Route("/", name="welcome")
      */
     public function index()
     {
-        return $this->render('welcome/index.html.twig', [
-            'controller_name' => 'WelcomeController',
+        return $this->render('welcome/index.html.twig');
+    }
+
+    /**
+     * @Route("/hello-page", name="hello_page")
+     */
+    public function hello(Request $request)
+    {
+        $name = $request->query->get('name', 'Stranger');
+        return $this->render('hello_page.html.twig', [
+            'name' => $name
         ]);
     }
 }
