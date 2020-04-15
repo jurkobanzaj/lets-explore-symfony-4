a1245136e99f989e2b17b7fa9511d7e932f8cb7b
Yuriy Piddubnyak <banzaj@gmail.com>
2020-04-15 16:31:59 +0300

    With send mail now

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,16 +12,30 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request)
+    public function contact(Request $request, \Swift_Mailer $mailer)
     {
         $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
 
+        $this->addFlash('info', 'some info');
+
         if ($form->isSubmitted() && $form->isValid()) {
             $contactFormData = $form->getData();
 
             dump($contactFormData);
+
+            $message = (new \Swift_Message('You Got Mail!'))
+                ->setFrom($contactFormData['email'])
+                ->setTo('banzaj@gmail.com')
+                ->setBody($contactFormData['message'], 'text/plain');
+
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Your message was sent');
+
+            return $this->redirectToRoute('contact');
+
         }
 
         return $this->render('contact/contact.html.twig', [
